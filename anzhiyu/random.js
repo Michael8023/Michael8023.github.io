a9c4cba32c112e76545180a80ba740fae50298db
Michael8023 <michael_liu1@126.com>
2022-11-01 16:50:35 +0800

    Site updated: 2022-11-01 16:50:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["post/233853f2.html","post/4a17b156.html","post/a206aef0.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["post/a206aef0.html","post/233853f2.html","post/4a17b156.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
