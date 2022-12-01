7711e8f6395a9c57dd3ac01216d49268d1c203c8
Michael8023 <michael_liu1@126.com>
2022-12-01 16:14:33 +0800

    Site updated: 2022-12-01 16:14:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["post/a7c623a1.html","post/2003bde4.html","post/233853f2.html","post/4a17b156.html","post/a206aef0.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["post/a7c623a1.html","post/2003bde4.html","post/4a17b156.html","post/233853f2.html","post/a206aef0.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
